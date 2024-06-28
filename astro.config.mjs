5fec1e2f4d9b543e281533bc5f3705fa814bec8b
sergio jimenez <sjr.social@gmail.com>
2024-06-28 14:34:22 +0200

    Añadida isla con Preact y boton de cambio de tema

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://primer-blog-astro.netlify.app",
+  integrations: [preact()]
 });
